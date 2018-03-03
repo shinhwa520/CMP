26f82cdad05383458a4968dd0b92a3566061b386
USER <USER@DESKTOP-9ETVA8F>
2018-03-03 14:01:55 +0800

    20180303_1400 user,cust model

--- src/com/cmp/model/Customer.java
+++ src/com/cmp/model/Customer.java
@@ -1,9 +1,7 @@
 package com.cmp.model;
 
 import java.sql.Timestamp;
-import java.text.SimpleDateFormat;
 import java.util.Date;
-import java.util.List;
 
 import javax.persistence.Column;
 import javax.persistence.Entity;
@@ -13,12 +11,12 @@ import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.JoinColumn;
 import javax.persistence.ManyToOne;
-import javax.persistence.OneToMany;
 import javax.persistence.Table;
 
 @Entity
 @Table(name="CUSTOMER")
 public class Customer implements java.io.Serializable {
+	private static final long serialVersionUID = 1L;
 
 	@Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
@@ -28,11 +26,41 @@ public class Customer implements java.io.Serializable {
     @Column(name = "cust_name", nullable = false)
 	private String name;
 
-    @Column(name = "city", nullable = true)
-	private String city;
+    @Column(name = "gender", nullable = true)
+	private String gender;
+    
+    @Column(name = "birthday", nullable = true)
+	private Date birthday;
     
     @Column(name = "phone", nullable = true)
 	private String phone;
+    
+    @Column(name = "email", nullable = true)
+	private String email;
+    
+    @Column(name = "weChat", nullable = true)
+	private String weChat;
+
+    @Column(name = "identity1_id", nullable = true)
+	private Integer identity1_id;
+    
+    @Column(name = "identity1_code", nullable = true)
+	private String identity1_code;
+    
+    @Column(name = "identity2_id", nullable = true)
+	private Integer identity2_id;
+    
+    @Column(name = "identity2_code", nullable = true)
+	private String identity2_code;
+    
+    @Column(name = "census", nullable = true)
+	private String census;
+    
+    @Column(name = "city", nullable = true)
+	private String city;
+    
+    @Column(name = "address", nullable = true)
+	private String address;
     
     @ManyToOne(fetch = FetchType.EAGER)
     @JoinColumn(name = "status_id", nullable = true)
@@ -54,27 +82,14 @@ public class Customer implements java.io.Serializable {
     @Column(name = "update_by", nullable = true)
 	private String updateBy;
     
-    @Column(name = "address", nullable = true)
-	private String address;
-    
-    @Column(name = "birthday", nullable = true)
-	private Date birthday;
-    
-    @Column(name = "gender", nullable = true)
-	private String gender;
-    
-    @Column(name = "email", nullable = true)
-	private String email;
-    
-    @Column(name = "weChat", nullable = true)
-	private String weChat;
     
 	public Customer() {
 	}
-
+	
 	public Customer(int id, String name, String city, String phone, Status status, User user, Timestamp createTime,
 			String createBy, Timestamp updateTime, String updateBy, String address, Date birthday, String gender,
-			String email, String weChat) {
+			String email, String weChat, Integer identity1_id, String identity1_code, Integer identity2_id,
+			String identity2_code, String census) {
 		super();
 		this.id = id;
 		this.name = name;
@@ -91,8 +106,13 @@ public class Customer implements java.io.Serializable {
 		this.gender = gender;
 		this.email = email;
 		this.weChat = weChat;
+		this.identity1_id = identity1_id;
+		this.identity1_code = identity1_code;
+		this.identity2_id = identity2_id;
+		this.identity2_code = identity2_code;
+		this.census = census;
 	}
-
+	
 	public int getId() {
 		return id;
 	}
@@ -211,5 +231,45 @@ public class Customer implements java.io.Serializable {
 
 	public void setWeChat(String weChat) {
 		this.weChat = weChat;
+	}
+
+	public Integer getIdentity1_id() {
+		return identity1_id;
+	}
+
+	public void setIdentity1_id(Integer identity1_id) {
+		this.identity1_id = identity1_id;
+	}
+
+	public String getIdentity1_code() {
+		return identity1_code;
+	}
+
+	public void setIdentity1_code(String identity1_code) {
+		this.identity1_code = identity1_code;
+	}
+
+	public Integer getIdentity2_id() {
+		return identity2_id;
+	}
+
+	public void setIdentity2_id(Integer identity2_id) {
+		this.identity2_id = identity2_id;
+	}
+
+	public String getIdentity2_code() {
+		return identity2_code;
+	}
+
+	public void setIdentity2_code(String identity2_code) {
+		this.identity2_code = identity2_code;
+	}
+
+	public String getCensus() {
+		return census;
+	}
+
+	public void setCensus(String census) {
+		this.census = census;
 	}
 }
--- src/com/cmp/model/User.java
+++ src/com/cmp/model/User.java
@@ -15,22 +15,7 @@ import javax.persistence.Transient;
 @Entity
 @Table(name="user")
 public class User implements java.io.Serializable {
-	
-	public User() {
-	}
-	
-	public User(String email, Role role, Status status) {
-		super();
-		Date current = new Date();
-		this.id = String.valueOf(current.getTime());
-		this.email = email;
-		this.role = role;
-		this.status = status;
-		this.createBy = "REG";
-		this.createDateTime = current;
-		this.updateBy = "REG";
-		this.updateDateTime = current;
-	}
+	private static final long serialVersionUID = 1L;
 
 	@Id
 //    @GeneratedValue(strategy = GenerationType.IDENTITY)
@@ -60,9 +45,15 @@ public class User implements java.io.Serializable {
 	@Column(name = "email", nullable = true)
 	private String email;
     
+    @Column(name = "weChat", nullable = true)
+	private String weChat;
+	
     @ManyToOne(fetch = FetchType.EAGER)
     @JoinColumn(name = "channel_id", nullable = true)
     private User channel;
+    
+    @Column(name = "reward", nullable = true)
+	private Integer reward;
     
     @Column(name = "create_by", nullable = true)
 	private String createBy;
@@ -94,7 +85,43 @@ public class User implements java.io.Serializable {
 	@Transient
 	private int _volume=0;		//實際成交量
 	
+	
+	public User() {
+	}
+	
+	public User(String email, Role role, Status status) {
+		super();
+		Date current = new Date();
+		this.id = String.valueOf(current.getTime());
+		this.email = email;
+		this.role = role;
+		this.status = status;
+		this.createBy = "REG";
+		this.createDateTime = current;
+		this.updateBy = "REG";
+		this.updateDateTime = current;
+	}
 
+	public User(String id, String name, String account, String password, Role role, Status status, String phone,
+			String email, String weChat, User channel, Integer reward, String createBy, Date createDateTime,
+			String updateBy, Date updateDateTime) {
+		super();
+		this.id = id;
+		this.name = name;
+		this.account = account;
+		this.password = password;
+		this.role = role;
+		this.status = status;
+		this.phone = phone;
+		this.email = email;
+		this.weChat = weChat;
+		this.channel = channel;
+		this.reward = reward;
+		this.createBy = createBy;
+		this.createDateTime = createDateTime;
+		this.updateBy = updateBy;
+		this.updateDateTime = updateDateTime;
+	}
 
 	public User(String id, String name, String account, String password, Role role, Status status, String phone,
 			String email, User channel, String createBy, Date createDateTime, String updateBy, Date updateDateTime) {
@@ -112,12 +139,6 @@ public class User implements java.io.Serializable {
 		this.createDateTime = createDateTime;
 		this.updateBy = updateBy;
 		this.updateDateTime = updateDateTime;
-	}
-
-	public User(String name, String password, String... roles) {
-		this.name = name;
-		this.password = password;
-		this.roles = roles;
 	}
 
 	public String getId() {
